b3f9b0d96b4ba5b677a67360e0c10b10c13b5c4c
enaburekhan <ericenaburekhan2018@gmail.com>
2024-09-25 22:49:57 +0100

    Add ordering functionality to project list

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
     before_action :set_project, only: [:show, :edit, :update, :destroy]
 
     def index
-      @projects = Project.all
+      @projects = Project.order(updated_at: :desc)
     end
 
     def show
