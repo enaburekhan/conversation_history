f9704c4b571d1bff83bb28f13055226cb0f05bae
enaburekhan <ericenaburekhan2018@gmail.com>
2024-09-29 23:07:09 +0100

    Fix style navbar positioning on large screens

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 
